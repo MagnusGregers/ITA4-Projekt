75b23c3b9cce546ff3bc8bb0184e699ee7f09a7e
MagnusGregers <you@example.com>
2025-05-12 10:48:35 +0200

    Fixed CreateDb script, fixed ID's in csv,

--- db/CreateDb.js
+++ db/CreateDb.js
@@ -22,27 +22,35 @@ const dbResult = await db.query('select now()');
 
 //drop table if exists, is to drop old tables and only keep active or excisting tables
 
-await db.query(`
-  drop table if exists carbon_footprint,
-  drop table if exists country,
-  drop table if exists gdp,
-  drop table if exists carbon_cap
+/*await db.query(`
+  drop table if exists carbon_footprint;
+  drop table if exists country;
+  drop table if exists gdp;
+  drop table if exists carbon_cap;
   `);
+  */
+
+  await db.query(`
+    drop table if exists carbon_cap;
+    drop table if exists gdp;
+    drop table if exists country;
+    drop table if exists carbon_footprint;
+    `)
 
 //Carbon Footprint Table
 await db.query(`
     create table carbon_footprint (
         transport_method text,
         code integer,
-        year date,
-        transport_emissions_pr_km integer
+        year int,
+        transport_emissions_pr_km numeric
     );
 `);
 
 //Country Table
 await db.query(`
   create table country (
-  country_id integer unique not null primary key autoincrement,
+  country_id integer unique not null primary key,
   country text
   );
 `);
@@ -50,24 +58,20 @@ await db.query(`
 //GDP Table
 await db.query(`
     create table gdp (
-       gdp_id integer unique not null primary key autoincrement,
-       country_id integer references country (country_id),
        country text,
-       code integer,
-       year date,
-       gdp_pr_capital integer
+       code text,
+       year numeric,
+       gdp_pr_capital numeric
     );
 `);
 
 //Carbon Capital Table
 await db.query(` 
     create table carbon_cap (
-        carbon_cap_id integer unique not null primary key autoincrement,
-        country_id integer references country (country_id),
         country text,
-        code integer,
-        year date,
-        pr_capita_co2_emissions integer
+        code text,
+        year numeric,
+        pr_capita_co2_emissions numeric
     );
 `);
 
@@ -84,19 +88,19 @@ await upload (
   'db/country.csv',
   'copy country (country_id, country) from stdin with csv header' 
 );
-await db.end();
 
   //GDP Table
   await upload (
     db,
     'db/gdp.csv',
-    'copy gdp (gdp_id, country_id, country, code, year, gdp_pr_capital) from stdin with csv header' 
+    'copy gdp (country, code, year, gdp_pr_capital) from stdin with csv header' 
   );
 
   //Carbon Capital Table
   await upload (
     db,
     'db/carbon_cap.csv',
-    'copy carbon_cap (carbon_cap_id, country_id, country, code, year, pr_capita_co2_emissions) from stdin with csv header' 
+    'copy carbon_cap (country, code, year, pr_capita_co2_emissions) from stdin with csv header' 
   );
 
+await db.end();
