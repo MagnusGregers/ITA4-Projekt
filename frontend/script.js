357bf742a8e39960cc8744c4424d38ca0dfeb2af
EmiliePoulsen <eaa24eamp@students.eaaa.dk>
2025-05-12 10:35:48 +0200

    sat task ind i script.js klar til programming!

---
+++ frontend/script.js
@@ -0,0 +1,53 @@
+
+//----------------animation (waves)--------------//
+
+// - Wave model
+
+// - wave animation
+
+//------------Graph------------------//
+
+// - header and text
+
+// - bar chart race (carbon_cap)
+
+// - bar chart race (gdp)
+
+// - dropdown button for each year
+
+//-------------animation (plaine)-------------//
+
+// - plaine model
+
+// - smoke or cloude model
+
+// - animation
+
+//---------------World map-----------------//
+
+// - Map
+
+// - fun fact box
+
+// - button for shoing each continent and countries
+
+// - hover effekt on the map
+
+// - dropdown button for choosing a specific year
+
+//-----------------memory game--------------------//
+
+// - ship model
+
+// - truck model
+
+// - cards
+
+// - text
+
+// - turning animation
+
+// - try again button
+
+// - game over and wind scrren when game is done
+
